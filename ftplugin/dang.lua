bd366c5cbbee800bf00204a24e70ef7545a5779e
Alex Suraci <suraci.alex@gmail.com>
2026-04-09 16:38:56 -0400

    ftplugin: don't require treesitter
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- ftplugin/dang.lua
+++ ftplugin/dang.lua
@@ -1,6 +1,13 @@
 -- Tree-sitter features
-vim.treesitter.start()
-vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+local has_parser = pcall(vim.treesitter.language.inspect, 'dang')
+if has_parser then
+  pcall(vim.treesitter.start, 0, 'dang')
+end
+
+local has_nvim_treesitter = pcall(require, 'nvim-treesitter')
+if has_parser and has_nvim_treesitter then
+  vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+end
 
 -- Buffer options
 vim.bo.commentstring = '# %s'

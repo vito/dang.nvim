3ea0df81e921c8d28421c1cb2499ee8afce10c0f
Alex Suraci <suraci.alex@gmail.com>
2026-06-02 16:13:59 -0400

    refactor: suffix declaration nodes with _decl
    The Dang grammar made its declaration rule names uniform, so the
    tree-sitter nodes interface/enum/scalar became interface_decl/enum_decl/
    scalar_decl. Update the highlight query to match.
    
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- queries/dang/highlights.scm
+++ queries/dang/highlights.scm
@@ -172,7 +172,7 @@
 ;; Type definitions
 (object_decl (symbol) @type)
 (implements (symbol) @type)
-(interface (symbol) @type)
-(enum (symbol) @type)
-(enum (caps_symbol) @property)
-(scalar (symbol) @type)
+(interface_decl (symbol) @type)
+(enum_decl (symbol) @type)
+(enum_decl (caps_symbol) @property)
+(scalar_decl (symbol) @type)
